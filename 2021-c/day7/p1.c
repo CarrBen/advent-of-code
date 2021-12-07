032cd3321754f8928efab565b4b4b586df6c0546
Ben Carr <CarrBen@users.noreply.github.com>
2021-12-07 18:42:25 +0000

    Day 7. Part 1

---
+++ 2021-c/day7/p1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define POSITION_COUNT (4096)
+
+int main(int argc, char* argv[])
+{
+	FILE* file;
+	int maxLineLength = 4096;
+	char buffer[maxLineLength];
+	char* filename = argv[1];
+
+	file = fopen(filename, "r");
+	int crabCount = 0;
+	int positionCounts[POSITION_COUNT];
+	memset(&positionCounts, 0, POSITION_COUNT * sizeof(int));
+	fgets(buffer, maxLineLength, file);
+	
+	char *token = strtok(buffer, ",");
+	while(token) {
+		int index = -1;
+		sscanf(token, "%d", &index);
+		positionCounts[index]++;
+		crabCount++;
+		token = strtok(NULL, ",");
+	}
+	
+	for (int i=0; i<10; i++) {
+		printf("%d,", positionCounts[i]);
+	}
+	printf("\n");
+
+	printf("Found %d crabs in %s.\n", crabCount, filename);
+
+	int moveLeftCost[POSITION_COUNT];
+	memset(&moveLeftCost, 0, POSITION_COUNT * sizeof(int));
+	
+	int moveRightCost[POSITION_COUNT];
+	memset(&moveRightCost, 0, POSITION_COUNT * sizeof(int));
+	
+	int movingCrabs = 0;
+	for (int i=1; i<POSITION_COUNT; i++) {
+		movingCrabs += positionCounts[i-1];
+		moveRightCost[i] = movingCrabs + moveRightCost[i-1];
+	}
+	
+	movingCrabs = 0;
+	for (int i=POSITION_COUNT-2; i>=0; i--) {
+		movingCrabs += positionCounts[i+1];
+		moveLeftCost[i] = movingCrabs + moveLeftCost[i+1];
+	}
+	
+	int minFuel = INT_MAX;
+	for (int i=0; i<POSITION_COUNT; i++) {
+		int sum = moveRightCost[i] + moveLeftCost[i];
+		if (sum < minFuel) {
+			printf("%d\t%2d - %2d\n", i, moveRightCost[i], moveLeftCost[i]);
+			minFuel = sum;
+		}
+	}
+	
+	printf("Min fuel cost %d\n", minFuel);
+
+	return 0;
+}
